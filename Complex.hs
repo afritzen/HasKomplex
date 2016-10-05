9faa18d3e03efa50a98748baeda2471db3f204eb
Anna Fritzen <afritzen@techfak.uni-bielefeld.de>
2016-10-05 14:02:17 +0200

    added standard operations and conjugation

--- Complex.hs
+++ Complex.hs
@@ -17,5 +17,38 @@ complex2 = Complex (-4) 2
 complexToString :: Complex -> String
 complexToString (Complex r i) | i >= 0 = show r ++ "+" ++ show i ++ "i"
 						   | otherwise = show r ++ show i ++ "i"
+			
+-- calculates the absolute value of a single complex number			
+complexAbs :: Complex -> Int
+complexAbs (Complex a b) = round . sqrt $ fromIntegral $ a^2 + b^2  
+complexAbs _ = error "invalid number!"
+
+-- conjugates a complex number by flipping the algebraic sign
+complexConjugate :: Complex -> Complex
+complexConjugate (Complex a b) = (Complex a (-b))
+complexConjugate _ = error "invalid number!"
+						   
+-- adds two complex numbers
+complexAdd :: Complex -> Complex -> Complex
+complexAdd (Complex a b) (Complex c d) = (Complex (a + c) (b + d))
+complexAdd _ _ = error "invalid number(s)!"
+
+-- subtracts two complex numbers
+complexSub :: Complex -> Complex -> Complex
+complexSub (Complex a b) (Complex c d) = (Complex (a - c) (b - d))
+complexSub _ _ = error "invalid number(s)!" 
+
+-- multiplies two complex numbers
+complexMult :: Complex -> Complex -> Complex
+complexMult (Complex a b) (Complex c d) = (Complex (a*c - b*d) (a*d + b*c))
+complexMult _ _ = error "invalid number(s)!"
+
+-- divides two complex numbers
+complexDiv :: Complex -> Complex -> Complex
+complexDiv (Complex a b) (Complex c d) = (Complex (a*c + b*d `div` c^2 + d^2) (b*c - a*d `div` c^2 + d^2))
+complexDiv _ _ = error "invalid number(s)!"
+
+
+
 						   
 
