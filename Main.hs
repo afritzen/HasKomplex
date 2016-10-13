c090fa5d0a41a0387056d048186194d0376ce949
Anna Fritzen <afritzen@techfak.uni-bielefeld.de>
2016-10-13 14:59:51 +0200

    implemented all operations in Main, finished general input processing

--- Main.hs
+++ Main.hs
@@ -12,20 +12,47 @@ main = do
 	putStrLn "What would you like to do?"
 	choice <- getLine
 	case choice of
-		"s" -> processSingleOperation
-		"d" -> processDoubleOperation
-		"q" -> exitSuccess
-		"h" -> putStrLn "--HELPTEXT HERE--"
-		otherwise -> main
+	    "s" -> processSingleOperation
+	    "d" -> processDoubleOperation
+	    "q" -> exitSuccess
+	    "h" -> putStrLn "--HELPTEXT HERE--"
+	    _   -> main
 	main
 
 -- processes a single-number-input and performs calculations
-processSingleOperation :: IO ()
-processSingleOperation = do
-			  putStrLn "TODO"
-
--- processes a two-number-input and performs calculations
 processDoubleOperation :: IO ()
 processDoubleOperation = do
-			  putStrLn "TODO"
-	
+	putStrLn "Please enter two complex numbers:"
+	fstNumberReal <- getLine
+	fstNumberImg <- getLine
+	sndNumberReal <- getLine
+	sndNumberImg <- getLine
+	let fstNumber = toComplex (read fstNumberReal) (read fstNumberImg)
+	let sndNumber = toComplex (read sndNumberReal) (read sndNumberImg)
+	putStrLn "Choose an operation (+, -, *, /, abs):"
+	opCode <- getLine
+	case opCode of
+	  "+" -> putStrLn $ complexToString $ complexAdd fstNumber sndNumber
+	  "-" -> putStrLn $ complexToString $ complexSub fstNumber sndNumber
+	  "*" -> putStrLn $ complexToString $ complexMult fstNumber sndNumber
+	  "/" -> putStrLn $ complexToString $ complexDiv fstNumber sndNumber
+	  "q" -> main
+	  _   -> putStrLn "Invalid operation code!" >> processDoubleOperation
+	main
+		
+-- processes a two-number-input and performs calculations
+processSingleOperation :: IO ()
+processSingleOperation = do
+	putStrLn "Please enter a complex number:"
+	numberReal <- getLine
+	numberImg <- getLine
+	let number = toComplex (read numberReal) (read numberImg)
+	putStrLn "Choose an operation (k, p):"
+	opCode <- getLine
+	case opCode of
+		"k" -> putStrLn $ complexToString $ complexConjugate number
+		"p" -> putStrLn "IMPLEMENT!"
+		"q" -> main
+		_   -> putStrLn "Invalid operation code!" >> processSingleOperation
+	main
+ 
