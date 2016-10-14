1dad3a32d467eae48d37ad4319b5051a451f8b2f
Anna Fritzen <afritzen@techfak.uni-bielefeld.de>
2016-10-14 18:06:41 +0200

    polar conversion, fixed number format, finished Main.hs

--- Complex.hs
+++ Complex.hs
@@ -2,6 +2,8 @@
 
 module Complex where
 
+import Data.Maybe
+
 data Complex = Complex Int Int
 			   deriving (Show, Eq, Ord, Read)
 
@@ -23,34 +25,54 @@ complexToString (Complex r i) | i >= 0 = show r ++ "+" ++ show i ++ "i"
 						   | otherwise = show r ++ show i ++ "i"
 			
 -- calculates the absolute value of a single complex number			
-complexAbs :: Complex -> Int
-complexAbs (Complex a b) = round . sqrt $ fromIntegral $ a^2 + b^2  
+complexAbs :: Complex -> Double
+complexAbs (Complex a b) = sqrt $ fromIntegral $ a^2 + b^2  
 complexAbs _ = error "invalid number!"
 
+-- evaluates the argument phi of a complex number
+getArgument :: Complex -> Double
+getArgument (Complex a b)
+	| abs == 0 = 0
+	| b >= 0 = acos (fromIntegral b / abs)
+	| otherwise = (-1) * (acos (fromIntegral b / abs))
+	where abs = complexAbs (Complex a b)
+	      arg = getArgument (Complex a b)
+getArgument _ = error "invalid number!"
+
+-- converts a complex number into the polar form and returns the sin and cos values
+-- of it's parts
+complexToPolar :: Complex -> (Double, Double, Double)
+complexToPolar (Complex a b) = (abs, cos arg, sin arg)
+					where arg = getArgument (Complex a b)
+					      abs = complexAbs (Complex a b)
+						  
+-- converts a triple of absolute amount, sinus(phi) and cosinus(phi) to a complex number
+polarToComplex :: (Double, Double, Double) -> Complex
+polarToComplex (abs, cosPhi, sinPhi) = (Complex (round (abs * cosPhi)) (round (abs * sinPhi)))
+						  
+-- prints out the polar form of a complex number
+printPolar :: (Double, Double, Double) -> String
+printPolar (abs, cosPhi, sinPhi) = "(" ++ show abs ++ "," ++ show cosPhi ++ "," ++ show sinPhi ++ ")"
+				  
 -- conjugates a complex number by flipping the algebraic sign
 complexConjugate :: Complex -> Complex
-complexConjugate (Complex a b) = (Complex a (-b))
-complexConjugate _ = error "invalid number!"
+complexConjugate (Complex a b) = Complex a (-b)
 						   
 -- adds two complex numbers
 complexAdd :: Complex -> Complex -> Complex
 complexAdd (Complex a b) (Complex c d) = (Complex (a + c) (b + d))
-complexAdd _ _ = error "invalid number(s)!"
 
 -- subtracts two complex numbers
 complexSub :: Complex -> Complex -> Complex
 complexSub (Complex a b) (Complex c d) = (Complex (a - c) (b - d))
-complexSub _ _ = error "invalid number(s)!" 
 
 -- multiplies two complex numbers
 complexMult :: Complex -> Complex -> Complex
 complexMult (Complex a b) (Complex c d) = (Complex (a*c - b*d) (a*d + b*c))
-complexMult _ _ = error "invalid number(s)!"
 
 -- divides two complex numbers
 complexDiv :: Complex -> Complex -> Complex
 complexDiv (Complex a b) (Complex c d) = (Complex (a*c + b*d `div` c^2 + d^2) (b*c - a*d `div` c^2 + d^2))
-complexDiv _ _ = error "invalid number(s)!"
 
 
 
--- Main.hs
+++ Main.hs
@@ -8,34 +8,32 @@ import System.Exit (exitSuccess)
 
 main :: IO ()
 main = do 
-	putStrLn "**WELCOME TO HasKomplex!**"
-	putStrLn "What would you like to do?"
+	putStrLn "\n**WELCOME TO HasKomplex!**"
+	putStrLn "\nWhat would you like to do ('s' - single operation, 'd' - double operation, \n'h' for help, 'q' to quit)?"
 	choice <- getLine
 	case choice of
 	    "s" -> processSingleOperation
 	    "d" -> processDoubleOperation
-	    "q" -> exitSuccess
-	    "h" -> putStrLn "--HELPTEXT HERE--"
+	    "q" -> putStrLn "Qutting ..." >> exitSuccess
+	    "h" -> putStrLn "\nTo enter a complex number, type 'Complex' followed by \nthe real and imaginary part as separated numbers.\nNegative numbers must be enclosed in brackets."
 	    _   -> main
 	main
 
 -- processes a single-number-input and performs calculations
 processDoubleOperation :: IO ()
 processDoubleOperation = do
-	putStrLn "Please enter two complex numbers:"
-	fstNumberReal <- getLine
-	fstNumberImg <- getLine
-	sndNumberReal <- getLine
-	sndNumberImg <- getLine
-	let fstNumber = toComplex (read fstNumberReal) (read fstNumberImg)
-	let sndNumber = toComplex (read sndNumberReal) (read sndNumberImg)
-	putStrLn "Choose an operation (+, -, *, /, abs):"
+	putStrLn "\nPlease enter two complex numbers:"
+	fstNumberInput <- getLine
+	sndNumberInput <- getLine
+	let fstNumber = read fstNumberInput
+	let sndNumber = read sndNumberInput
+	putStrLn "\nChoose an operation (+, -, *, /, abs):"
 	opCode <- getLine
 	case opCode of
-	  "+" -> putStrLn $ complexToString $ complexAdd fstNumber sndNumber
-	  "-" -> putStrLn $ complexToString $ complexSub fstNumber sndNumber
-	  "*" -> putStrLn $ complexToString $ complexMult fstNumber sndNumber
-	  "/" -> putStrLn $ complexToString $ complexDiv fstNumber sndNumber
+	  "+" -> putStrLn "\n" >> (putStrLn $ complexToString $ complexAdd fstNumber sndNumber) 
+	  "-" -> putStrLn "\n" >> (putStrLn $ complexToString $ complexSub fstNumber sndNumber)
+	  "*" -> putStrLn "\n" >> (putStrLn $ complexToString $ complexMult fstNumber sndNumber)
+	  "/" -> putStrLn "\n" >> (putStrLn $ complexToString $ complexDiv fstNumber sndNumber)
 	  "q" -> main
 	  _   -> putStrLn "Invalid operation code!" >> processDoubleOperation
 	main
@@ -43,15 +41,14 @@ processDoubleOperation = do
 -- processes a two-number-input and performs calculations
 processSingleOperation :: IO ()
 processSingleOperation = do
-	putStrLn "Please enter a complex number:"
-	numberReal <- getLine
-	numberImg <- getLine
-	let number = toComplex (read numberReal) (read numberImg)
-	putStrLn "Choose an operation (k, p):"
+	putStrLn "\nPlease enter a complex number:"
+	numberInput <- getLine
+	let number = read numberInput
+	putStrLn "\nChoose an operation (k, p):"
 	opCode <- getLine
 	case opCode of
-		"k" -> putStrLn $ complexToString $ complexConjugate number
-		"p" -> putStrLn "IMPLEMENT!"
+		"k" -> putStrLn "\n" >> (putStrLn $ complexToString $ complexConjugate number)
+		"p" -> putStrLn "\n" >> (putStrLn $ printPolar $ complexToPolar number)
 		"q" -> main
 		_   -> putStrLn "Invalid operation code!" >> processSingleOperation
 	main
